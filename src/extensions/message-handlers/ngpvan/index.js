e3f9979c3dd8fdc380772e3a79022689f7574b3d
Adam Greenspan <agreenspan24@gmail.com>
2020-08-25 21:36:51 -0500

    NGP_VAN_INITIAL_REPLY_CANVASS_RESULT

--- src/extensions/message-handlers/ngpvan/index.js
+++ src/extensions/message-handlers/ngpvan/index.js
@@ -2,6 +2,7 @@ import { getConfig } from "../../../server/api/lib/config";
 const Van = require("../../../extensions/action-handlers/ngpvan-action");
 import { log } from "../../../lib";
 import { getActionChoiceData } from "../../../extensions/action-handlers";
+import { cacheableData } from "../../../server/models";
 
 export const DEFAULT_NGP_VAN_INITIAL_TEXT_CANVASS_RESULT = "Texted";
 
@@ -31,8 +32,29 @@ export const postMessageSave = async ({ contact, organization, campaign }) => {
     return {};
   }
 
-  if (contact.message_status !== "needsMessage") {
-    return {};
+  const initialReplyCanvassResult = getConfig(
+    "NGP_VAN_INITIAL_REPLY_CANVASS_RESULT",
+    organization
+  );
+
+  let canvassResult;
+  if (contact.message_status === "needsMessage") {
+    canvassResult =
+      getConfig("NGP_VAN_INITIAL_TEXT_CANVASS_RESULT", organization) ||
+      DEFAULT_NGP_VAN_INITIAL_TEXT_CANVASS_RESULT;
+  } else if (
+    initialReplyCanvassResult &&
+    contact.message_status === "needsResponse"
+  ) {
+    const messages =
+      (await cacheableData.message.query({ campaignContactId: contact.id })) ||
+      [];
+
+    // First message not from the contact will be initial text, the second is the initial reply since the user can only send one initial text.
+    if (messages.filter(m => !m.is_from_contact).length === 2) {
+      console.log({ initialReplyCanvassResult });
+      canvassResult = initialReplyCanvassResult;
+    }
   }
 
   const clientChoiceData = await getActionChoiceData(
@@ -44,7 +66,7 @@ export const postMessageSave = async ({ contact, organization, campaign }) => {
     getConfig("NGP_VAN_INITIAL_TEXT_CANVASS_RESULT", organization) ||
     DEFAULT_NGP_VAN_INITIAL_TEXT_CANVASS_RESULT;
 
-  const texted = clientChoiceData.find(ccd => ccd.name === initialTextResult);
+  const texted = clientChoiceData.find(ccd => ccd.name === canvassResult);
   const body = JSON.parse(texted.details);
 
   return Van.postCanvassResponse(contact, organization, body, campaign)
