6ff85b0b77c32fc43f467ca3eb8ca04f4d4e1e68
Adam Greenspan <agreenspan24@gmail.com>
2020-10-18 10:51:45 -0600

    Revert actions

--- src/server/api/canned-response.js
+++ src/server/api/canned-response.js
@@ -1,20 +1,18 @@
 import { mapFieldsToModel } from "./lib/utils";
-import { CannedResponse, CannedResponseAction, cacheableData } from "../models";
+import { CannedResponse, CannedResponseAction } from "../models";
 
 export const resolvers = {
   CannedResponse: {
     ...mapFieldsToModel(["id", "title", "text"], CannedResponse),
     isUserCreated: cannedResponse => cannedResponse.user_id !== "",
-    actions: async (cannedResponse, _, { user }) => {
-      const campaignCannedResponses = await cacheableData.cannedResponse.query({
-        campaignId: cannedResponse.campaign_id
+    actions: async cannedResponse => {
+      if (cannedResponse.hasOwnProperty("actions")) {
+        return cannedResponse.actions;
+      }
+
+      return CannedResponseAction.getAll(cannedResponse.id, {
+        index: "canned_response_id"
       });
-
-      const cachedCannedResponse = campaignCannedResponses.find(
-        c => c.id === cannedResponse.id
-      );
-
-      return cachedCannedResponse.actions;
     }
   },
   CannedResponseAction: {
